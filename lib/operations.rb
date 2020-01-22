14b29d25380d700899ad61653eed4057acaf8533
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 04:07:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,12 +11,6 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60
-    return true
-  elsif speed < 40
-    return true
-  else speed > 40 && speed < 60
-    return false
-  end
+  speed > 60 || speed < 40 ? true : false
 
 end
